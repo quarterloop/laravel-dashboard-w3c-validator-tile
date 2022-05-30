7de51cae07cb0f70e3b61626b7df637d05d543b4
Gero Wallisch <gero.wallisch@me.com>
2022-05-30 19:08:22 +0200

    Build service provider

---
+++ src/w3cValidatorTileServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Quarterloop\DNSTile;
+
+use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
+use Quarterloop\w3cValidatorTile\Commands\Fetchw3cValidatorCommand;
+
+class w3cValidatorTileServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Fetchw3cValidatorCommand::class,
+            ]);
+        }
+
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/dashboard-w3c-validator-tile'),
+        ], 'dashboard-w3c-validator-views');
+
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'dashboard-w3c-validator-tile');
+
+        Livewire::component('w3c-validator-tile', w3cValidatorTileComponent::class);
+    }
+}
